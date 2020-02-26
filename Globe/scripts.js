347316564f73c3f11688066f5ecc9b26cba4a73e
pdspatrick <pdspatrick@gmail.com>
2020-02-26 17:03:20 -0600

    added some comments

--- Globe/scripts.js
+++ Globe/scripts.js
@@ -1,15 +1,17 @@
 function doubleGlobe(lat, lon, divtype1, divtype2){
+    /* I hate this solution but it works so...*/
     var globe = initialize(lat,lon, divtype1);
     var fakeglobe = initialize(lat,lon, divtype2);
     return [globe, fakeglobe]
 }
+
 function initialize(lati, long, divtype) {
     var earth = new WE.map(divtype, options);
     var lat = lati
     var lon = long
     var options = {atmosphere: true, center: [lat, lon], zoom: 0};
     var marker = WE.marker([lat, lon]).addTo(earth);
- WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       minZoom: 0,
       maxZoom: 25,
       attribution: 'OpenStreetMap'
@@ -23,10 +25,12 @@ function addPopup(lat, lon, popstring, earth){
 }
                               
 function flyToFood(lat, lon, zoom, earth, fakeearth) {
+    /* Using the second globe to determine the layout bounds to pan to with a shaweet animation */
     earth.panInsideBounds(fakeFindPort(lat, lon, zoom, fakeearth));
       }
 
 function fakeFindPort(lat, lon, zoom, fakeglobe){
+    /* Offset is needed if we want to fit the popup window on screen */
     var offsetlat = .005
     var offsetlon = .000
     fakeglobe.setView([(lat + offsetlat), (lon + offetlon)], zoom);
